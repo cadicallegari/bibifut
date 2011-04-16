bcbe6555e3e26c93d79bef70ee683e7220c2a8ec
rockhoundi@gmail.com <rockhoundi@gmail.com@50602b53-6266-bedd-f44f-667cfcffe961>
2011-04-16 12:36:53 +0000

    doido velho

--- biblioteca/src/menu.c
+++ biblioteca/src/menu.c
@@ -4,55 +4,40 @@
 
 void showMenu (struct lista_livro **head){
 
-    int x;
+    int op;
 
     do {
-        printf ("\n\nSISTEMA DE CADASTRO DE LIVROS\n\n");
-        printf ("1. CADASTRAR LIVRO\n");
-        printf ("2. REMOVER LIVRO\n");
-        printf ("3. LISTA DE LIVROS CADASTRADOS");
-        printf ("4. EMPRESTIMO\n");
-        printf ("5. DEVOLUCAO\n");
-        printf ("6. LISTA DE LIVOS EM ATRASO\n");
-        printf ("0. SAIR\n");
+    	op = showOptions();
 
-        scanf ("%d", &x);
+        switch (op){
+			case 1 : *head = cadastraLivro(head) ; break ;
+			case 2 : printf ("\n2");break;
+			case 3 : imprimeListaLivros(head) ;break;
+			case 4 : printf ("\n4");break;
+			case 5 : printf ("\n5");break;
+			case 6 : printf ("\n6");break;
+			case 0 : break;
+			default : printf ("Opcao Invalida!"); break;
+        }
 
-        struct lista_livro *aux = *head;
-        selecionaOp (aux, x);
-
-    } while ( x!=0 );
-
-
-
+    } while (op!=0 );
 
 }
 
+int showOptions() {
 
-void selecionaOp ( struct lista_livro **head, int x){
+	int x = 0;
 
+	printf ("\n\nSISTEMA DE CADASTRO DE LIVROS\n\n");
+	printf ("1. CADASTRAR LIVRO\n");
+	printf ("2. REMOVER LIVRO\n");
+	printf ("3. LISTA DE LIVROS CADASTRADOS\n");
+	printf ("4. EMPRESTIMO\n");
+	printf ("5. DEVOLUCAO\n");
+	printf ("6. LISTA DE LIVOS EM ATRASO\n");
+	printf ("0. SAIR\n");
 
-    switch (x){
+    scanf ("%d", &x);
 
-        case 1 : cadastraLivro(&head) ; break ;
-
-        case 2 : printf ("\n2");break;
-
-        case 3 : imprimeListaLivros(head) ;break;
-
-        case 4 : printf ("\n4");break;
-
-        case 5 : printf ("\n5");break;
-
-        case 6 : printf ("\n6");break;
-
-        case 0 : break;
-
-        default : printf ("Opcao Invalida!"); break;
-
-}
-
-
-
-
+    return x;
 }
